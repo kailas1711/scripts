8cf3a00a87f6a19616b3a114f9de0455b3170488
aws institute <kailas1711@gmail.com>
2019-04-28 09:59:20 +0530

    testing

---
+++ test.py
@@ -0,0 +1,7 @@
+11. Common Work Flow :
+a) Create  a branch from desired feature
+git branch calc-divide (creates the branch from master)
+git branch (check the branch)
+git checkout calc-divide ( move from master to the created branch)
+b) Make changes to the calc.py in divide function
+
